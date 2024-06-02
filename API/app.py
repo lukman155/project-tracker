6cadbbfa586ba414805a82220bf72e266b593318
dkeemz <abdulhakeem.abg@gmail.com>
2024-06-02 13:12:02 +0100

    Added Projects Endpoint

--- API/app.py
+++ API/app.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask
+from flask import Flask, request, make_response, jsonify
 from datetime import datetime
 from flask_restful import Api, Resource
 
@@ -22,7 +22,7 @@ class User(db.Model):
     username = db.Column(db.String(50), unique=True, nullable=False)
     password_hash = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.UTC)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 class Project(db.Model):
@@ -59,7 +59,7 @@ class CreateTables(Resource):
         db.create_all()
 
 
-class Users(Resource):
+class GetUsers(Resource):
     def get(self):
         users = User.query.all()
         user_list = []
@@ -70,7 +70,77 @@ class Users(Resource):
         return {'users': user_list}, 200
 
 
-# Add API endpoint
-api.add_resource(CreateTables,'/api/create')
-api.add_resource(Users, "/api/users")
+class GetProjects(Resource):
+    def get(self):
+        projects = Project.query.all()
+        Project_list = []
+        for project in projects:
+            project_data = {"id": project.id, 'name': project.name, 'description': project.description,
+                            'category': project.category, 'gps_location': project.gps_location,
+                            "owner_id": project.owner_id,
+                            'created_at': project.created_at, 'updated_at': project.updated_at}
+            Project_list.append(project_data)
+        return {"projects": Project_list}, 200
 
+
+class AddUser(Resource):
+    def post(self):
+        if request.is_json:
+            b = User(name=request.json['username'], email=request.json['email'], password=request.json['password_hash'],
+                     created_at=request.json['created_at'])
+            db.session.add(b)
+            db.session.commit()
+            return make_response(jsonify(
+                {id: b.id, 'username': b.username, 'email': b.email, 'password_hash': b.password}), 201)
+        else:
+            return {"error": "error must be JSON"}
+
+
+class AddProject(Resource):
+    def post(self):
+        if request.is_json:
+            b = Project(name=request.json['name'], description=request.json['description'],
+                        category=request.json['category'], gps_location=request.json['gps_location'],
+                        owner_id=request.json['owner_id'], created_at=request.json['created_at'],
+                        updated_at=request.json['updated_at'])
+            db.session.add(b)
+            db.session.commit()
+            return make_response(jsonify({"id": b.id, "name": b.name, "location": b.location, "user_id": b.user_id}),
+                                 201)
+        else:
+            return {"error": "error must be JSON"}
+
+
+class DeleteUser(Resource):
+    def delete(self, id):
+        if request.is_json:
+            user = User.query.get(id)
+            if user is None:
+                return {'error': 'Not Found'}, 404
+            db.session.delete(user)
+            db.session.commit()
+            return f"user with ID:{id} is deleted"
+
+
+# class UpdateUser(Resource):
+#     def put(self, id):
+#         if request.is_json:
+#             user = User.query.get(id)
+#             if user is None:
+#                 return {'error': 'Not Found'}, 404
+#             else:
+#                 user.first_name = request.json['username']
+#                 user.last_name = request.json['last_name']
+#                 user.project = request.json['project']
+#
+#                 db.session.commit()
+#                 return f"updated", 200
+#         else:
+#             return {"error": "error must be JSON"}
+
+
+# Add API endpoint
+api.add_resource(CreateTables, '/api/create')
+api.add_resource(GetUsers, "/api/users")
+api.add_resource(GetProjects, "/api/projects")
+api.add_resource(AddProject, "/api/add/project")

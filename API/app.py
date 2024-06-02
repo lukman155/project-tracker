1225ed3e07086276fd02f11654fb7bd7a05b62f2
dkeemz <abdulhakeem.abg@gmail.com>
2024-06-02 12:51:06 +0100

    Added Users Endpoint

--- API/app.py
+++ API/app.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask import Flask
 from datetime import datetime
+from flask_restful import Api, Resource
 
 # Assuming you have already created your Flask app instance
 app = Flask(__name__)
@@ -9,8 +10,10 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+api = Api(app)
 # Create an instance of SQLAlchemy
 db = SQLAlchemy(app)
+
 
 # Define SQLAlchemy models
 
@@ -20,6 +23,7 @@ class User(db.Model):
     password_hash = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.UTC)
+
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -31,6 +35,7 @@ class Project(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+
 class Report(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
@@ -40,9 +45,32 @@ class Report(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow)
     due_date = db.Column(db.DateTime)
-    
+
+
 # Define UserRoles as a separate table
 class UserRoles(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     role = db.Column(db.String(20), nullable=False)
+
+
+class CreateTables(Resource):
+    def get(self):
+        db.create_all()
+
+
+class Users(Resource):
+    def get(self):
+        users = User.query.all()
+        user_list = []
+        for user in users:
+            user_data = {"id": user.id, 'username': user.username, 'password_hash': user.password_hash,
+                         'email': user.email, 'created_at': user.created_at}
+            user_list.append(user_data)
+        return {'users': user_list}, 200
+
+
+# Add API endpoint
+api.add_resource(CreateTables,'/api/create')
+api.add_resource(Users, "/api/users")
+

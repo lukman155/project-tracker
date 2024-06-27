6b2e71e6ae36cd16d0d256f38fed4c10a1c8b174
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2024-06-27 23:15:42 +0100

    Update AuthContext.jsx

---
+++ client/src/context/AuthContext.jsx
@@ -0,0 +1,23 @@
+// src/contexts/AuthContext.js
+import React, { createContext, useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+
+export const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decodedToken = jwt_decode(token);
+      setUser(decodedToken);
+    }
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};

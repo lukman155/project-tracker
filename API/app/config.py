e0f7d33f748431b3dc9dd88d418ead977a7bc224
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2024-06-28 15:24:20 +0100

    Add- Email Verication API

--- API/app/config.py
+++ API/app/config.py
@@ -8,8 +8,15 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///project_tracker.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     JWT_TOKEN_LOCATION = ['cookies']
     JWT_COOKIE_SECURE = True
     JWT_COOKIE_CSRF_PROTECT = True
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(days=30)
+    
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
